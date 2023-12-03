21bdbb4d61e38f319f7274105ec50276e8511d77
acJoaog <124536687+acJoaog@users.noreply.github.com>
2023-12-03 18:23:09 -0300

    Update spec1.cy.js

--- Cypress/cypress/e2e/spec1.cy.js
+++ Cypress/cypress/e2e/spec1.cy.js
@@ -14,23 +14,11 @@ describe('Testando sites no cypress', () => {
     cy.get('#nameofuser').should('contain.text', 'Welcome asa')
   })
 
-<<<<<<< Updated upstream
-  it('Testando Login com senha errada', () => {
-    let info = cadastro()
-    cy.visit('https://www.demoblaze.com/index.html')
-    cy.get('#login2').click()
-    cy.wait(2000)
-    cy.get('#loginpassword').type('senha')
-    cy.get('#loginusername').type(info[0])
-    cy.wait(2000)
-    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
-=======
   it('Testando recuperacao de email invalido', () => {
     cy.visit('https://the-internet.herokuapp.com/forgot_password')
     cy.get('#email').type('email')
     cy.get('#form_submit').click()
     cy.get('h1').should('contain.text', 'Internal Server Error')
->>>>>>> Stashed changes
   })
 
 })
